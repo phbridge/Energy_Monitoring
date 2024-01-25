1d00084bc0cb7c01ebe63c4f140400c94387a39d
phbridge <bridges.p.j1@gmail.com>
2024-01-25 21:13:16 +0000

    deviding by 10 from temp changing fan speed/kwh bits.

--- mqtt-to-influx.py
+++ mqtt-to-influx.py
@@ -149,6 +149,14 @@ def on_message(client, userdata, msg):
                              }
         function_logger.debug(json_message)
         if json_message["msg"] == "ENVIRONMENTAL-CURRENT-SENSOR-DATA":
+            HOSTS_DB["DysonFans"][serial]["tact"] = str(round((int(json_message["data"]["tact"])/10) - 273.15, 2))
+            HOSTS_DB["DysonFans"][serial]["hact"] = json_message["data"]["hact"]
+            HOSTS_DB["DysonFans"][serial]["pm25"] = json_message["data"]["pm25"]
+            HOSTS_DB["DysonFans"][serial]["pm10"] = json_message["data"]["pm10"]
+            HOSTS_DB["DysonFans"][serial]["va10"] = json_message["data"]["va10"]
+            HOSTS_DB["DysonFans"][serial]["noxl"] = json_message["data"]["noxl"]
+            HOSTS_DB["DysonFans"][serial]["p25r"] = json_message["data"]["p25r"]
+            HOSTS_DB["DysonFans"][serial]["p10r"] = json_message["data"]["p10r"]
             string = "AirQuality,fan=%s,serial=%s " \
                      "temp=%s,humidity=%s," \
                      "pm25=%s,pm10=%s," \
@@ -159,6 +167,8 @@ def on_message(client, userdata, msg):
                       json_message["data"]["pm25"], json_message["data"]["pm10"],
                       json_message["data"]["va10"], json_message["data"]["noxl"],
                       json_message["data"]["p25r"], json_message["data"]["p10r"])
+
+
             return string
         elif json_message["msg"] == "CURRENT-STATE":
             string = "FanPower,fan=%s,serial=%s " \
@@ -341,6 +351,25 @@ def request_fan_data_thread():
         return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
 
     def _request_fan_data(serial):
+        future_speed = 0
+        if HOSTS_DB["DysonFans"][serial]["pm25"] > 250.5 or HOSTS_DB["DysonFans"][serial]["pm10"] > 420.5 or HOSTS_DB["DysonFans"][serial]["va10"] > 8.5 or HOSTS_DB["DysonFans"][serial]["noxl"] > 8.5:
+            future_speed = 9  # this is the "RED" level for most
+        elif HOSTS_DB["DysonFans"][serial]["pm25"] > 150.5 or HOSTS_DB["DysonFans"][serial]["pm10"] > 350.5:
+            future_speed += 4
+        elif HOSTS_DB["DysonFans"][serial]["pm25"] > 70.5 or HOSTS_DB["DysonFans"][serial]["pm10"] > 100.5:
+            future_speed += 3
+        elif HOSTS_DB["DysonFans"][serial]["pm25"] > 53.5 or HOSTS_DB["DysonFans"][serial]["pm10"] > 75.5 or HOSTS_DB["DysonFans"][serial]["va10"] > 6.5 or HOSTS_DB["DysonFans"][serial]["noxl"] > 6.5:
+            future_speed += 2 # this is the "ORANGE" level for most
+        elif HOSTS_DB["DysonFans"][serial]["pm25"] > 35.5 or HOSTS_DB["DysonFans"][serial]["pm10"] > 50.5 or HOSTS_DB["DysonFans"][serial]["va10"] > 3.5 or HOSTS_DB["DysonFans"][serial]["noxl"] > 3.5:
+            future_speed += 1  # this is the "YELLOW" level for most
+        else:
+            future_speed = 1  # this is the "GREEN" level for most
+
+        if not HOSTS_DB["DysonFans"][serial]["current_speed"] == future_speed:
+            HOSTS_DB["DysonFans"][serial]["device_object"].set_speed = future_speed
+
+        function_logger.critical("%s - %s - %s" % (HOSTS_DB["DysonFans"][serial]["name"], HOSTS_DB["DysonFans"][serial]["current_speed"], future_speed))
+
         function_logger.debug("requesting fan data for %s" % serial)
         topic = "438/%s/command" % serial
         payload = {
