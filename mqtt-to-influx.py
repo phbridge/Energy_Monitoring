db41c0934ba69cb98c6dd04bbdf66681c45fccde
phbridge <bridges.p.j1@gmail.com>
2024-01-25 19:58:51 +0000

    multiple changes to monitor DysonFan enviromental data and power usage

--- mqtt-to-influx.py
+++ mqtt-to-influx.py
@@ -12,8 +12,9 @@ import subprocess
 import inspect
 import credentials
 import requests
-from datetime import timedelta          # calculate x time ago
-from datetime import datetime           # timestamps mostly
+import time
+# from datetime import timedelta          # calculate x time ago
+# from datetime import datetime           # timestamps mostly
 
 
 ABSOLUTE_PATH = credentials.ABSOLUTE_PATH
@@ -130,9 +131,60 @@ def on_message(client, userdata, msg):
     # print(msg.topic + " " + str(msg.payload))
     # print(msg.topic + " " + str(bytes(msg.payload)))
     topic_tree = str(msg.topic).split("/")
-    time_slot = datetime.now()
+    time_slot = datetime.datetime.now()
 
-    def proceess_plug_power(plugname, message_type, message):
+    def _process_fan_data(serial, message):
+        json_message = json.loads(message)
+        fan_speed_mapping = {0: 2.2,
+                             1: 4.8,
+                             2: 5.4,
+                             3: 6.4,
+                             4: 7.0,
+                             5: 10.8,
+                             6: 14.6,
+                             7: 20.3,
+                             8: 27.5,
+                             9: 37.0,
+                             10: 46.2
+                             }
+        # print(PRICE_KWH)
+        print(json_message)
+        if json_message["msg"] == "ENVIRONMENTAL-CURRENT-SENSOR-DATA":
+            string = "AirQuality,fan=%s,serial=%s " \
+                     "temp=%s,humidity=%s," \
+                     "pm25=%s,pm10=%s," \
+                     "voc=%s,nox=%s," \
+                     "p25r=%s,p10r=%s" % \
+                     (HOSTS_DB["DysonFans"][serial]["name"], serial,
+                      str(round(int(json_message["msg"]["data"]["tact"]) - 273.15, 2)), json_message["msg"]["data"]["hact"],
+                      json_message["msg"]["data"]["pm25"], json_message["msg"]["data"]["pm10"],
+                      json_message["msg"]["data"]["va10"], json_message["msg"]["data"]["noxl"],
+                      json_message["msg"]["data"]["p25r"], json_message["msg"]["data"]["p10r"])
+            print(json_message)
+            print(string)
+        elif json_message["msg"] == "CURRENT-STATE":
+            string = "FanPower,fan=%s,serial=%s " \
+                     "fanspeed=%s,fanpower=%s," \
+                     "cost=%s" % \
+                     (HOSTS_DB["DysonFans"][serial]["name"], serial,
+                      json_message["msg"]["product-state"]["fnsp"], fan_speed_mapping[json_message["msg"]["product-state"]["fnsp"]],
+                      str(fan_speed_mapping[json_message["msg"]["product-state"]["fnsp"]] * (PRICE_KWH/1000))
+                      )
+            print(string)
+            print(json_message)
+        elif json_message["msg"] == "STATE-CHANGE":
+            string = "FanPower,fan=%s,serial=%s " \
+                     "fanspeed=%s,fanpower=%s," \
+                     "cost=%s" % \
+                     (HOSTS_DB["DysonFans"][serial]["name"], serial,
+                      json_message["msg"]["product-state"]["fnsp"],
+                      fan_speed_mapping[json_message["msg"]["product-state"]["fnsp"]],
+                      str(fan_speed_mapping[json_message["msg"]["product-state"]["fnsp"]] * (PRICE_KWH / 1000))
+                      )
+            print(string)
+            print(json_message)
+
+    def _process_plug_power(plugname, message_type, message):
         if message_type == "STATE":
             STATE_json = json.loads(message)
             # function_logger.info(out11_json)
@@ -169,83 +221,95 @@ def on_message(client, userdata, msg):
             HOSTS_DB["LocalBytes_plugs"][plugname]["last_power"] = SENSOR_json["ENERGY"]["Total"]
 
             cost = power_rate * PRICE_KWH
+            cost_absolute = Power * (PRICE_KWH/1000)
 
             influx_upload = "LocalBytes_plugs,plug_name=%s " \
-                            "Power=%s,ApparentPower=%s,ReactivePower=%s,Factor=%s,Voltage=%s,Current=%s,cost=%s,power_rate=%s \n" % \
-                            (plugname, Power, ApparentPower, ReactivePower, Factor, Voltage, Current, cost, power_rate)
+                            "Power=%s,ApparentPower=%s,ReactivePower=%s,Factor=%s,Voltage=%s,Current=%s,cost=%s,cost_absolute=%s,power_rate=%s \n" % \
+                            (plugname, Power, ApparentPower, ReactivePower, Factor, Voltage, Current, cost, cost_absolute, power_rate)
             return influx_upload
-        elif message_type == "STATUS9":
-            out9_json = json.loads(message)
-            PowerLow = out9_json["StatusPTH"]["PowerLow"]
-            PowerHigh = out9_json["StatusPTH"]["PowerHigh"]
-            VoltageLow = out9_json["StatusPTH"]["VoltageLow"]
-            VoltageHigh = out9_json["StatusPTH"]["VoltageHigh"]
-            CurrentLow = out9_json["StatusPTH"]["CurrentLow"]
-            CurrentHigh = out9_json["StatusPTH"]["CurrentHigh"]
-            influx_upload = "LocalBytes_plugs,plug_name=%s " \
-                             "PowerLow=%s,PowerHigh=%s,VoltageLow=%s,VoltageHigh=%s,CurrentLow=%s,CurrentHigh=%s \n" % \
-                             (plugname,PowerLow, PowerHigh, VoltageLow, VoltageHigh, CurrentLow, CurrentHigh,)
-            return influx_upload
-        elif message_type == "STATUS10":
-            out10_json = json.loads(message)
-            Total = out10_json["StatusSNS"]["ENERGY"]["Total"]
-            Power = out10_json["StatusSNS"]["ENERGY"]["Power"]
-            ApparentPower = out10_json["StatusSNS"]["ENERGY"]["ApparentPower"]
-            ReactivePower = out10_json["StatusSNS"]["ENERGY"]["ReactivePower"]
-            Factor = out10_json["StatusSNS"]["ENERGY"]["Factor"]
-            Voltage = out10_json["StatusSNS"]["ENERGY"]["Voltage"]
-            Current = out10_json["StatusSNS"]["ENERGY"]["Current"]
-            if not HOSTS_DB["LocalBytes_plugs"][plugname].get("last_power"):
-                HOSTS_DB["LocalBytes_plugs"][plugname]["last_power"] = out10_json["StatusSNS"]["ENERGY"]["Total"]
-            difference_in_power = out10_json["StatusSNS"]["ENERGY"]["Total"] - HOSTS_DB["LocalBytes_plugs"][plugname][
-                "last_power"]
-            if not HOSTS_DB["LocalBytes_plugs"][plugname].get("last_power_time"):
-                HOSTS_DB["LocalBytes_plugs"][plugname]["last_power_time"] = time_slot
-            difference_in_time = (time_slot - HOSTS_DB["LocalBytes_plugs"][plugname]["last_power_time"]).seconds
-            HOSTS_DB["LocalBytes_plugs"][plugname]["last_power_time"] = time_slot
-            if difference_in_time > 0:
-                power_rate = difference_in_power / (int(difference_in_time) / 3600)
-            else:
-                power_rate = 0
-            HOSTS_DB["LocalBytes_plugs"][plugname]["last_power"] = out10_json["StatusSNS"]["ENERGY"]["Total"]
-
-            cost = power_rate * PRICE_KWH
-
-            influx_upload = "LocalBytes_plugs,plug_name=%s " \
-                             "Power=%s,ApparentPower=%s,ReactivePower=%s,Factor=%s,Voltage=%s,Current=%s,cost=%s,power_rate=%s \n" % \
-                             (plugname, Power, ApparentPower, ReactivePower, Factor, Voltage, Current, cost, power_rate)
-            return influx_upload
-        elif message_type == "STATUS11":
-            out11_json = json.loads(message)
-            # function_logger.info(out11_json)
-            UptimeSec = out11_json["StatusSTS"]["UptimeSec"]
-            Heap = out11_json["StatusSTS"]["Heap"]
-            influx_upload = "LocalBytes_plugs,plug_name=%s " \
-                             "uptimesec=%s,Heap=%s \n" % \
-                             (plugname, UptimeSec, Heap)
-            return influx_upload
+        # elif message_type == "STATUS9":
+        #     out9_json = json.loads(message)
+        #     PowerLow = out9_json["StatusPTH"]["PowerLow"]
+        #     PowerHigh = out9_json["StatusPTH"]["PowerHigh"]
+        #     VoltageLow = out9_json["StatusPTH"]["VoltageLow"]
+        #     VoltageHigh = out9_json["StatusPTH"]["VoltageHigh"]
+        #     CurrentLow = out9_json["StatusPTH"]["CurrentLow"]
+        #     CurrentHigh = out9_json["StatusPTH"]["CurrentHigh"]
+        #     influx_upload = "LocalBytes_plugs,plug_name=%s " \
+        #                      "PowerLow=%s,PowerHigh=%s,VoltageLow=%s,VoltageHigh=%s,CurrentLow=%s,CurrentHigh=%s \n" % \
+        #                      (plugname,PowerLow, PowerHigh, VoltageLow, VoltageHigh, CurrentLow, CurrentHigh,)
+        #     return influx_upload
+        # elif message_type == "STATUS10":
+        #     out10_json = json.loads(message)
+        #     Total = out10_json["StatusSNS"]["ENERGY"]["Total"]
+        #     Power = out10_json["StatusSNS"]["ENERGY"]["Power"]
+        #     ApparentPower = out10_json["StatusSNS"]["ENERGY"]["ApparentPower"]
+        #     ReactivePower = out10_json["StatusSNS"]["ENERGY"]["ReactivePower"]
+        #     Factor = out10_json["StatusSNS"]["ENERGY"]["Factor"]
+        #     Voltage = out10_json["StatusSNS"]["ENERGY"]["Voltage"]
+        #     Current = out10_json["StatusSNS"]["ENERGY"]["Current"]
+        #     if not HOSTS_DB["LocalBytes_plugs"][plugname].get("last_power"):
+        #         HOSTS_DB["LocalBytes_plugs"][plugname]["last_power"] = out10_json["StatusSNS"]["ENERGY"]["Total"]
+        #     difference_in_power = out10_json["StatusSNS"]["ENERGY"]["Total"] - HOSTS_DB["LocalBytes_plugs"][plugname][
+        #         "last_power"]
+        #     if not HOSTS_DB["LocalBytes_plugs"][plugname].get("last_power_time"):
+        #         HOSTS_DB["LocalBytes_plugs"][plugname]["last_power_time"] = time_slot
+        #     difference_in_time = (time_slot - HOSTS_DB["LocalBytes_plugs"][plugname]["last_power_time"]).seconds
+        #     HOSTS_DB["LocalBytes_plugs"][plugname]["last_power_time"] = time_slot
+        #     if difference_in_time > 0:
+        #         power_rate = difference_in_power / (int(difference_in_time) / 3600)
+        #     else:
+        #         power_rate = 0
+        #     HOSTS_DB["LocalBytes_plugs"][plugname]["last_power"] = out10_json["StatusSNS"]["ENERGY"]["Total"]
+        #
+        #     cost = power_rate * PRICE_KWH
+        #
+        #     influx_upload = "LocalBytes_plugs,plug_name=%s " \
+        #                      "Power=%s,ApparentPower=%s,ReactivePower=%s,Factor=%s,Voltage=%s,Current=%s,cost=%s,power_rate=%s \n" % \
+        #                      (plugname, Power, ApparentPower, ReactivePower, Factor, Voltage, Current, cost, power_rate)
+        #     return influx_upload
+        # elif message_type == "STATUS11":
+        #     out11_json = json.loads(message)
+        #     # function_logger.info(out11_json)
+        #     UptimeSec = out11_json["StatusSTS"]["UptimeSec"]
+        #     Heap = out11_json["StatusSTS"]["Heap"]
+        #     influx_upload = "LocalBytes_plugs,plug_name=%s " \
+        #                      "uptimesec=%s,Heap=%s \n" % \
+        #                      (plugname, UptimeSec, Heap)
+        #     return influx_upload
         else:
             function_logger.info("got unrecognied or unwanted PLUG message:%s topic:%s from:%s" % (message, str(topic_tree), plugname))
 
     try:
-        if topic_tree[0] == "energy":  # this will likly go plug when topics updated
-            if topic_tree[1] == "plugpower":
-                influx_upload = proceess_plug_power(topic_tree[2], topic_tree[3], msg.payload.decode('utf-8'))
-                function_logger.debug(influx_upload)
-                if update_influx(influx_upload):
-                    function_logger.debug(influx_upload)
-                else:
-                    function_logger.error("influx upload failed")
-        elif topic_tree[0] == "tele":
+        # if topic_tree[0] == "energy":  # this will likly go plug when topics updated
+        #     if topic_tree[1] == "plugpower":
+        #         influx_upload = proceess_plug_power(topic_tree[2], topic_tree[3], msg.payload.decode('utf-8'))
+        #         function_logger.debug(influx_upload)
+        #         if update_influx(influx_upload):
+        #             function_logger.debug(influx_upload)
+        #         else:
+        #             function_logger.error("influx upload failed")
+
+        if topic_tree[0] == "tele":  # this mostly handels the plugs
             if topic_tree[1] in HOSTS_DB["LocalBytes_plugs"].keys():
-                influx_upload = proceess_plug_power(plugname=topic_tree[1], message_type=topic_tree[2], message=msg.payload.decode('utf-8'))
+                influx_upload = _process_plug_power(plugname=topic_tree[1], message_type=topic_tree[2], message=msg.payload.decode('utf-8'))
                 function_logger.debug(influx_upload)
                 if update_influx(influx_upload):
                     function_logger.debug(influx_upload)
                 else:
                     function_logger.error("influx upload failed")
             else:
-                function_logger.warning("got message from unauthorised topic:%s message:%s" % (msg.topic, str(msg.payload)))
+                function_logger.warning("got message from unauthorised plug topic:%s message:%s" % (msg.topic, str(msg.payload)))
+        elif topic_tree[0] == "438":
+            if topic_tree[1] in HOSTS_DB["DysonFans"].keys():
+                influx_upload = _process_fan_data(serial=topic_tree[1], message=msg.payload.decode('utf-8'))
+                function_logger.debug(influx_upload)
+                if update_influx(influx_upload):
+                    function_logger.debug(influx_upload)
+                else:
+                    function_logger.error("influx upload failed")
+            else:
+                function_logger.warning("got message from unauthorised Fan topic:%s message:%s" % (msg.topic, str(msg.payload)))
         else:
             function_logger.info("got unrecognied or unwanted message topic:%s message:%s" % (msg.topic, str(msg.payload)))
     except IndexError as e:
@@ -265,8 +329,42 @@ def graceful_killer(signal_number, frame):
     function_logger.info("Got Kill signal")
     function_logger.info('Received:' + str(signal_number))
     THREAD_TO_BREAK.set()
+    request_fan_data.join()
     function_logger.info("set threads to break")
-    quit()
+
+
+def request_fan_data_thread():
+    function_logger = logger.getChild("%s.%s" % (inspect.stack()[1][3], inspect.stack()[0][3]))
+    function_logger.debug("starting equest_fan_data_thread")
+
+    def _mqtt_time():
+        """Return current time string for mqtt messages."""
+        return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
+
+    def _request_fan_data(serial):
+        function_logger.debug("requesting fan data for %s" % serial)
+        topic = "438/%s/command" % serial
+        payload = {
+            "msg": "REQUEST-PRODUCT-ENVIRONMENT-CURRENT-SENSOR-DATA",
+            "time": _mqtt_time(),
+        }
+        mqttc.publish(topic=topic, payload=json.dumps(payload))
+        payload = {
+            "msg": "REQUEST-CURRENT-STATE",
+            "time": _mqtt_time(),
+        }
+        mqttc.publish(topic=topic, payload=json.dumps(payload))
+        return
+
+    time.sleep(45)
+    while not THREAD_TO_BREAK.is_set():
+        start_time = datetime.datetime.now()
+        for serial in HOSTS_DB["DysonFans"].keys():
+            _request_fan_data(serial=serial)
+        next_time = start_time + datetime.timedelta(seconds=30)
+        function_logger.debug("sleeping for %s seconds" % (next_time - start_time).seconds)
+        if (next_time - start_time).seconds > 0:
+            THREAD_TO_BREAK.wait((next_time - start_time).seconds)
 
 
 def load_hosts_file_json():
@@ -308,14 +406,19 @@ if __name__ == '__main__':
     logger.info("__main__ - " + "GET_CURRENT_DB")
     HOSTS_DB = load_hosts_file_json()
     #
-    # thread_local_bytes_plugs = threading.Thread(target=lambda: master_local_bytes_plugs())
-    # thread_local_bytes_plugs.start()
+
     ## MQTT logic - Register callbacks and start MQTT client
     mqttc.on_connect = on_connect
     mqttc.on_message = on_message
 
+    # start sending data requests every 30 seconds
+    request_fan_data = threading.Thread(target=lambda: request_fan_data_thread())
+    request_fan_data.start()
+
     while not THREAD_TO_BREAK.is_set():
         mqttc.loop()
-    quit()
+
+    mqttc.disconnect()
+
 
 
